382c223b0b859dec5480def1f33444d8f3f07499
Yılmaz Edis <ylmazedis@gmail.com>
2022-12-15 23:11:14 +0300

    Getting ready for editing

---
+++ UIKit-Reminder/DetailViewController/ReminderViewController+Section.swift
@@ -0,0 +1,30 @@
+//
+//  ReminderViewController+Section.swift
+//  UIKit-Reminder
+//
+//  Created by yilmaz on 15.12.2022.
+//
+
+import Foundation
+
+extension ReminderViewController {
+    enum Section: Int, Hashable {
+        case view
+        case title
+        case date
+        case notes
+        
+        var name: String {
+            switch self {
+            case .view: return ""
+            case .title:
+                return NSLocalizedString("Title", comment: "Title section name")
+            case .date:
+                return NSLocalizedString("Date", comment: "Date section name")
+            case .notes:
+                return NSLocalizedString("Notes", comment: "Notes section name")
+            }
+        }
+    }
+}
+
